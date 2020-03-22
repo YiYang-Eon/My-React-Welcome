242b9bb5c6746357dac22bc13996e4dbe9f923e5
YiYang-Eon <15594999221@163.com>
2020-03-23 02:00:34 +0800

    first commit

---
+++ src/MyName.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './MyName.css'
+
+function MyName() {
+    return (
+        <div className="MyName">
+            <p className="MyName-test">Welcome! Yi Yang</p>
+        </div>
+    );
+}
+
+export default MyName;
